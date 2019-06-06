e592a531c4d5642452b5d6ac6ec196a0064be35d
James Cho <hwisoocho@gmail.com>
2019-06-06 13:38:18 -0700

    make api call and fetch data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const API_KEY = 'f68015cb4910abf208ca4d742ad9298f'
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+      //weather
+      weather: {},
+      weatherDetails: {},
+    };
+  }
+
+  
+
+  fetchWeather() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let zip = '98327';
+      let url =
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}`;
+  
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  handleWeatherData() {
+    let promise = this.fetchWeather();
+    promise.then(response => {
+      console.log((response));
+    
+    });
+  }
+  
+  componentDidMount(){
+    this.handleWeatherData()
+  }
+
+  render(){
+
+    return (
+      <div className="App">
       <header className="App-header">
+        <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React Weather App</h1>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://https://github.com/hwisoo/React-Weather-App-Challenge.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Source Code on Github
         </a>
       </header>
+      <button className="btn btn-primary">Button</button>
     </div>
   );
+}
 }
 
 export default App;
